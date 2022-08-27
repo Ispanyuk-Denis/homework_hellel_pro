bdf4919bc1e1f648728e561b4c3923ed375a0462
Ispanyuk-Denis <ispanukdenis@gmail.com>
2022-08-27 23:52:52 +0300

    added 9 lesson

---
+++ les9/js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+//1
+
+class Employee {
+
+    static vacation = 18;
+
+    constructor (firstName , lastName , age , jobPosition , salary ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.jobPosition = jobPosition;
+        this.salary = salary;
+    }
+
+    get fullName() {
+        console.log (`${this.firstName} ${this.lastName}`);
+    }
+
+    getSalaryOfYear() {
+        console.log (`Salary of year ${this.salary * 12}`);
+    }
+}
+
+const juniorEmployee = new Employee ('Taras', 'Shevchenko', 30, 'QA', 500);
+const seniorEmployee = new Employee ('Lesia', 'Ukrainka', 25, 'CEO', 800);
+console.log (juniorEmployee);
+console.log (seniorEmployee);
+seniorEmployee.fullName;
+console.log (Employee.vacation);
+juniorEmployee.getSalaryOfYear();
