479671c5f94699830db8028cf070a93df2081a25
Ispanyuk-Denis <ispanukdenis@gmail.com>
2022-09-02 00:18:34 +0300

    change filter to find

--- les12/js/script.js
+++ les12/js/script.js
@@ -7,6 +7,6 @@ const API_URL = "https://jsonplaceholder.typicode.com/users";
 fetch(API_URL)
 .then((response) => response.json())
 .then((result) => {
-const filterUser = result.filter(item => item.company.name === 'Johns Group');
-console.log(filterUser)})
+const searchedUser = result.find(item => item.company.name === 'Johns Group');
+console.log(searchedUser)})
 .catch((error) => console.log(error));
