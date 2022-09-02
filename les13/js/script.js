871ace4d1ab3d66856625c3f142aec5b853551fe
Ispanyuk-Denis <ispanukdenis@gmail.com>
2022-09-02 20:20:17 +0300

    added 13les

---
+++ les13/js/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+//1
+
+// const API_URL = "https://jsonplaceholder.typicode.com/users/1";
+// fetch(API_URL)
+// .then((response) => response.json())
+// .then((result) => {
+//     const dataUser = result;
+//     localStorage.setItem("obj", JSON.stringify(dataUser));
+// })
+// .then (() => {
+//     const localDataUser = JSON.parse(localStorage.getItem('obj'));
+//     if (localDataUser === null) {
+//         fetch(API_URL)
+//         .then((response) => response.json())
+//         .then((result) => {
+//             const dataUser = result;
+//             localStorage.setItem("obj", JSON.stringify(dataUser));
+//             }
+//         )
+//     }
+// })
+// .catch((error) => console.log(error));
+
+//2
+
+// function isValidDateFormat(str) {
+//     let regexp = /^\d\d\d\d\/\d\d\/\d\d \d\d\:\d\d$/
+//     return regexp.test(str)
+// }
+// console.log(isValidDateFormat("2012/09/18 12:10")); // true
+// console.log(isValidDateFormat("12.02.2022 12:10")); // false
+
+//3
+
+// function isAfter (startDate, finishDate) {
+//     if (typeof startDate === 'object' || typeof finishDate === 'object') {
+//         const start = startDate.getTime();
+//         const finish = finishDate.getTime();
+//         return (start > finish)
+//     }
+//     return (startDate > finishDate);
+// }
+// console.log(isAfter(new Date(1989, 6, 10), new Date(1987, 1, 11))); // true
+// console.log(isAfter(1648636135000, 1648549735000)); // true
+// console.log(isAfter(1648549735000, 1648636135000)); // false
